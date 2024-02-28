b4080b452422181b4bcbf27732f18d5456f6c9d8
Diego Fernandez <diegoafanador10@gmail.com>
2024-02-27 22:07:52 -0400

    [FEAT]testing request trigger by events

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -7,6 +7,9 @@ import { generateManyProducts } from 'src/app/models/app.mocks';
 import { defer, of } from 'rxjs';
 import { Product } from 'src/app/models/app.models';
 import { ValueService } from 'src/app/services/value/value.service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { HtmlParser } from '@angular/compiler';
 
 fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -74,6 +77,19 @@ fdescribe('ProductsComponent', () => {
       fixture.detectChanges()
       expect(component.status).toEqual('error')
     }))
+
+    it('should make request when button trigger', fakeAsync(() => {
+      const mockProducts = generateManyProducts(10)
+      productsService.getAll.and.returnValue(defer(() => Promise.resolve(mockProducts)))
+      const buttonDeb : DebugElement = fixture.debugElement.query(By.css('#btnProducts'))
+      buttonDeb.triggerEventHandler('click', null)
+      fixture.detectChanges()
+      expect(component.status).toEqual('loading')
+      tick()
+      fixture.detectChanges()
+      expect(component.status).toEqual('success')
+      expect(productsService.getAll).toHaveBeenCalled()
+    }))
   })
 
   describe('testing promise call', () => {
@@ -85,5 +101,16 @@ fdescribe('ProductsComponent', () => {
       expect(component.rta).toEqual(mockMsg)
       expect(valueService.getPromiseValue).toHaveBeenCalled()
     })
+
+    it('should exceute promise call when button trigger', fakeAsync (() => {
+      const mockMsg = 'new promise'
+      valueService.getPromiseValue.and.returnValue(Promise.resolve(mockMsg))
+      const buttonDeb : DebugElement = fixture.debugElement.query(By.css('#btnPromise'))
+      buttonDeb.triggerEventHandler('click', null)
+      tick()
+      fixture.detectChanges()
+      const pElem : HTMLElement = fixture.debugElement.query(By.css('p')).nativeElement
+      expect(pElem.textContent).toContain(mockMsg)
+    }))
   })
 });
