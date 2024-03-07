3ad8741cbfd9a227f2bc51502cd32a1e83aa6a23
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-07 14:25:58 -0400

    [FEAT]testing routes with params

--- src/app/products/product-detail/product-detail.component.spec.ts
+++ src/app/products/product-detail/product-detail.component.spec.ts
@@ -1,25 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDetailComponent } from './product-detail.component';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ActivatedRouteStub, mockObservable } from '@testing';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { generateOneProduct } from 'src/app/models/app.mocks';
 
-xdescribe('ProductDetailComponent', () => {
+fdescribe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
+  let productsService: jasmine.SpyObj<ProductsService>
+  let router: ActivatedRouteStub
+  let location: jasmine.SpyObj<Location>
 
   beforeEach(async () => {
+    const spyProducts = jasmine.createSpyObj<ProductsService>('ProductsService', ['getOne'])
+    const spyLocation = jasmine.createSpyObj<Location>('Location', ['back'])
+    let activeRouteSub = new ActivatedRouteStub()
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailComponent ]
+      declarations: [ ProductDetailComponent ],
+      providers: [
+        {provide: ProductsService, useValue: spyProducts},
+        {provide: ActivatedRoute, useValue: activeRouteSub},
+        {provide: Location, useValue: spyLocation}
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductDetailComponent);
+    productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>
+    location = TestBed.inject(Location) as jasmine.SpyObj<Location>
+    router = TestBed.inject(ActivatedRoute) as unknown as ActivatedRouteStub
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    const productId = '2'
+    router.setParamMap({id: productId})
+
+    const productMock = {
+      ...generateOneProduct(),
+      id: productId
+    }
+
+    productsService.getOne.and.returnValue(mockObservable(productMock))
+    fixture.detectChanges();//ngoninit
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render product', () => {
+
+  })
 });
--- src/testing-helpers/index.ts
+++ src/testing-helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './async-data'
 export * from './finders'
 export * from './forms'
+export * from './activated-route-stub'
