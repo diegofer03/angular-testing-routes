f95cbca3dda31dff8d797d4e8b49c1a5b0d51835
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-08 12:45:43 -0400

    [FEAT]testing routerLink in components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterLinkDirectiveStub, queryAllByDirective } from '@testing';
 
-xdescribe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let component : AppComponent
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterLinkDirectiveStub
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges()
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'ng-testing-services'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-testing-services');
-  });
+  it('should exist 7 routerlinks directives', () => {
+    const links = queryAllByDirective(fixture, RouterLinkDirectiveStub)
+    expect(links.length).toEqual(7)
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-testing-services app is running!');
-  });
+  it('should go to correct link', () => {
+    const links = queryAllByDirective(fixture, RouterLinkDirectiveStub)
+    const routes = links.map(rt => rt.injector.get(RouterLinkDirectiveStub))
+
+    expect(routes.length).toEqual(7)
+    expect(routes[0].linkParams).toEqual('/')
+    expect(routes[6].linkParams).toEqual('/others')
+  })
+
+  // it(`should have as title 'ng-testing-services'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('ng-testing-services');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('ng-testing-services app is running!');
+  // });
 });
